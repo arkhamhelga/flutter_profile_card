88fde7ac84c8bb79a7c16501ab3992aa7e196110
arkhamhelga <arkham.helga.taulana>
2025-10-17 09:14:18 +0700

    Belajar fundamental fluttter - debugging

---
+++ lib/doc.about.dart
@@ -0,0 +1,51 @@
+
+// ignore: dangling_library_doc_comments
+/** Penjelasan./
+ * struktuf flutter terdiri dari
+ * 1. .dart tool
+ * 2. .idea
+ * 3. android
+ *    => untuk konfigurasi anroid serta
+ *    => liblary android
+ * 4. build
+ *    => hasil compile aplikasi
+ * 5. ios
+ *    => untuk konfigurasi ios serta
+ *    => liblary ios(native)
+ * 6. lib
+ *    => tempat file dart berada yang nantinya
+ *    => sering digunakan
+ * 7. linux
+ *    => untuk konfigurasi linux
+ * 8. MacOs
+ *    => untuk konfigurasi MacOs
+ * 9. test
+ *    => tempat file untuk testing aplikasi
+ * 10. web
+ *    => untuk konfigurasi web
+ * 11. windows
+ *    => untuk konfigurasi windows
+ * 12. pusbspec.yaml
+ *    => tempat kofigurasi aplikasi
+ *    => menambahkan liblary
+ *    => menambahkan asset (gambar, font, dll)
+ * 
+ * Di terminal
+ * kegunaan perinta "flutter  pub get"
+ *    => untuk  mensyincron liblary yang ada di pubspec.yaml
+ * 
+ * * kegunaan perintah "flutter pub upgrade"
+ *    => untuk mengupdate liblary tang ada di pubspec.yaml
+ * 
+ * * kegunaan perintah "flutter clean"
+ *    => membersikan file build
+ * 
+ * liblary dari flutter bisa dilihat di pub.dev
+ * intl, http, button, dll
+ * 
+ * di dalam dart ada 2 statement
+ * 1. statless widget 
+ *    => widget yang tidak memiliki state (static)
+ * 2. steteful widget 
+ *    => widget yang memiliki state (dinamic)
+ */
